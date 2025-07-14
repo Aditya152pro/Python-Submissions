bbce532b8d4bece1dd47ae2b50d4e1ac4fae00be
Aditya152pro <adityasuvarna152@gmail.com>
2025-07-14 16:22:53 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,12 @@
+a= input('Enter the first number: ')
+b= input('Enter the second number: ')
+a = float(a)
+b = float(b)
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print(' The addition value is : ',c)
+print(' The subtraction value is : ',d)
+print(' The multiplication value is : ',e)
+print(' The division value is : ',f)
---
+++ Assignment_2.py
@@ -0,0 +1,3 @@
+a = input('Enter the first name : ')
+b = input('Enter your last name : ')
+print("Hello "+a +" "+b+"! Welcome to the Python Program")
