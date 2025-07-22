f8b7aa6e23d9b24dde54f566a3b457953d80e115
Aditya152pro <adityasuvarna152@gmail.com>
2025-07-22 11:45:52 +0530

    Add files via upload

---
+++ Math Module Calculator.py
@@ -0,0 +1,8 @@
+import math
+n = int(input("Enter a number: "))
+sresult = math.sqrt(n)
+print("Square root: ",sresult)
+lresult = math.log(n)
+print("Logarithm: ",lresult)
+siresult = math.sin(n)
+print("Sine: ",siresult)
